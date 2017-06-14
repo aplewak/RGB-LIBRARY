83f0af3f9d56835fe74218a811ee528e0140b34f
aplewak <andrewplewak@gmail.com>
2017-06-13 21:24:08 -0400

    Create RGB.h

--- RGB.h
+++ RGB.h
@@ -5,10 +5,13 @@ class RGB{
 
 	private:
 		int r,g,b;
+		int rp,gp,bp;
 	public:
 		void red();
 		void green();
 		void blue();
 		void customColor(int,int,int);
+		void setPins(int,int,int);
+		void getPins();
 };
 #endif
